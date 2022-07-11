8e6ad23b455ba936bc0b2471c1d4d47086bd1718
Chinmay Borah <chinmay205765@keshav.du.ac.in>
2022-07-11 15:19:47 +0530

    Input field prototype

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -1,12 +1,17 @@
 import { StyleSheet, TextInput, View } from "react-native";
 import { useState } from "react";
+import { colors } from "../constant/colors";
+import { Dimensions } from "react-native";
+
+const windowWidth = Dimensions.get('window').width
+const windowHeight = Dimensions.get('window').height
 
 export default function AuthScreen() {
     const [text, onChangeText] = useState("");
   return (
-    <View>
+    <View style = {styles.container}>
       <TextInput
-        style={styles.input}
+    style = {styles.inputField}
         onChangeText={onChangeText}
         value={text}
       />
@@ -15,11 +20,23 @@ export default function AuthScreen() {
 }
 
 const styles = StyleSheet.create({
-  input: {
-    marginTop:200,
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
+  container:{
+    flex:1,
+    backgroundColor:colors.black,
+    justifyContent:"center",
+    alignItems:"center"
   },
+  inputField:{
+    borderWidth:1.5,
+    borderRadius:200,
+    borderLeftColor:colors.yellow,
+    borderBottomColor:colors.yellow,
+    borderRightColor:colors.pink,
+    borderTopColor:colors.pink,
+    width:windowWidth-20,
+    height:windowHeight-(windowHeight*0.91),
+    backgroundColor:colors.black,
+    paddingLeft:16,
+    fontSize:22
+  }
 });
